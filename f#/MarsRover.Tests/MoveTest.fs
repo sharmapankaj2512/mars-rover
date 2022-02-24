89c7bc8fb2bc9af7e4d938eb0b151c0127bb0686
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-02-24 20:15:25 +0100

    :white_check_mark: move three steps to right from 0:0:E

--- f#/MarsRover.Tests/MoveTest.fs
+++ f#/MarsRover.Tests/MoveTest.fs
@@ -5,7 +5,8 @@ open Rover
 
 module MoveTest =
     [<Test>]
-    [<TestCase("RM", "1:0:E")>]    
+    [<TestCase("RM", "1:0:E")>]
+    [<TestCase("RMMM", "3:0:E")>]
     let moveRight (command: string, expectedPosition: string) =
         let rover = NewRover()
         let updatedRover = Navigate(rover, command)
