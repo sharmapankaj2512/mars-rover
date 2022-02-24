f08fb98119f317ddc2b031a55636678a88951469
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-02-24 11:38:42 +0100

    :test_tube: move left from 0:0:N

--- f#/MarsRover.Tests/RotateLeftTest.fs
+++ f#/MarsRover.Tests/RotateLeftTest.fs
@@ -9,6 +9,7 @@ module RotateLeftTest =
     [<TestCase("L", "0:0:W")>]
     [<TestCase("LL", "0:0:S")>]
     [<TestCase("LLL", "0:0:E")>]
+    [<TestCase("LLLL", "0:0:N")>]
     let RotateLeft (command: string, expectedPosition: string) =
         let rover = NewRover()
         let updatedRover = Navigate(rover, command)
