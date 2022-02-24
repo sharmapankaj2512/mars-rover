233ef8777a29fde2ee93d0693e92fbdf8e7e3a80
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-02-24 20:16:15 +0100

    :white_check_mark: move ten places to right

--- f#/MarsRover.Tests/MoveTest.fs
+++ f#/MarsRover.Tests/MoveTest.fs
@@ -7,6 +7,7 @@ module MoveTest =
     [<Test>]
     [<TestCase("RM", "1:0:E")>]
     [<TestCase("RMMM", "3:0:E")>]
+    [<TestCase("RMMMMMMMMMM", "10:0:E")>]
     let moveRight (command: string, expectedPosition: string) =
         let rover = NewRover()
         let updatedRover = Navigate(rover, command)
