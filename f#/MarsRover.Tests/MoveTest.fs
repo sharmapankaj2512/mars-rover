e65b7436505b449427a7f2493fcb8dd6b7d5162c
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-02-24 20:35:57 +0100

    :white_check_mark: move three steps to left from 10:0:W

--- f#/MarsRover.Tests/MoveTest.fs
+++ f#/MarsRover.Tests/MoveTest.fs
@@ -17,6 +17,7 @@ module MoveTest =
     [<TestCase("RMLLM", "0:0:W")>]
     [<TestCase("RMMLLMM", "0:0:W")>]
     [<TestCase("RMMLLM", "1:0:W")>]
+    [<TestCase("RMMMMMMMMMMLLMMM", "7:0:W")>]
     let moveLeft (command: string, expectedPosition: string) =
         let rover = NewRover()
         let updatedRover = Navigate(rover, command)
