a7dc1cf194447e78c9bcceebf317f5f075439222
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-02-24 14:02:21 +0100

    :test_tube: rotate right from 0:0:S

--- f#/MarsRover.Tests/RotateRightTest.fs
+++ f#/MarsRover.Tests/RotateRightTest.fs
@@ -7,6 +7,7 @@ module RotateRightTest =
     [<Test>]
     [<TestCase("R", "0:0:E")>]
     [<TestCase("RR", "0:0:S")>]
+    [<TestCase("RRR", "0:0:W")>]
     let RotateRight (command: string, expectedPosition: string) =
         let rover = NewRover()
         let updatedRover = Navigate(rover, command)
