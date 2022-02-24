c52d798c07d068fe56e5a31a410b191df1ab0285
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-02-24 09:41:47 +0100

    :recycle: rotate left from 0:0:W

--- f#/MarsRover.Tests/RotateLeftTest.fs
+++ f#/MarsRover.Tests/RotateLeftTest.fs
@@ -7,6 +7,7 @@ module RotateLeftTest =
 
     [<Test>]
     [<TestCase("L", "0:0:W")>]
+    [<TestCase("LL", "0:0:S")>]
     let RotateLeft (command: string, expectedPosition: string) =
         let rover = NewRover()
         let updatedRover = Navigate(rover, command)
--- f#/MarsRover.Tests/Rover.fs
+++ f#/MarsRover.Tests/Rover.fs
@@ -6,6 +6,7 @@ module Rover =
     type Direction =
         | North = 0
         | West = 1
+        | South = 2
 
     type Rover = { direction: Direction }
     let NewRover () = { direction = Direction.North }
@@ -14,10 +15,15 @@ module Rover =
         match rover.direction with
         | Direction.North -> "0:0:N"
         | Direction.West -> "0:0:W"
+        | Direction.South -> "0:0:S"
         | _ -> ArgumentOutOfRangeException() |> raise
-        
-    let Navigate (rover: Rover, command: string) =
-        if rover.direction = Direction.North then
-            {rover with direction = Direction.West}
+
+    let rec Navigate (rover: Rover, command: string) =
+        if command.Length = 0 then
+            rover
+        elif rover.direction = Direction.North then
+            Navigate({ rover with direction = Direction.West }, command.[1..])
+        elif rover.direction = Direction.West then
+            Navigate({ rover with direction = Direction.South }, command.[1..])
         else
             rover
