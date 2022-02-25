57f44879e6787f7088398530e3fd143e418c18d6
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-02-25 12:20:08 +0100

    :recycle: extract move right and move left behaviours

--- f#/MarsRover/Rover.fs
+++ f#/MarsRover/Rover.fs
@@ -15,12 +15,16 @@ module Rover =
         { rover with
               direction = Right(rover.direction) }
 
+    let moveRight (rover: Rover) =
+        { rover with
+              x = (rover.x + 1) % MoveLimit }
+
+    let moveLeft (rover: Rover) = { rover with x = rover.x - 1 }
+
     let move (rover: Rover) =
         match rover.direction with
-        | Direction.East ->
-            { rover with
-                  x = (rover.x + 1) % MoveLimit }
-        | Direction.West -> { rover with x = rover.x - 1 }
+        | Direction.East -> moveRight rover
+        | Direction.West -> moveLeft rover
 
     let Position (rover: Rover) =
         match rover.direction with
